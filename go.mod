ed8c2618a5ab195baa92ec88732a89e400d8e25f
Yu-Cheng,Chen <tigernaxo@gmail.com>
2025-05-29 01:46:18 +0000

    jump

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tigernaxoJr/blog
+
+go 1.24.3
+
+require github.com/hugo-toha/toha/v4 v4.9.0 // indirect
--- serve-public.sh
+++ serve-public.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-hugo serve -D --bind=0.0.0.0 --baseURL=https://0.0.0.0/
+hugo serve -wD --bind=0.0.0.0 --baseURL=https://0.0.0.0/
